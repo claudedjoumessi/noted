ecac489c5554c351a8f8c904f9df56d0a7d97970
claudedjoumessi <djoumessiclaude95@gmail.com>
2025-12-29 01:44:15 +0100

    refactor: simplify CommandPalette rendering and improve code readability

--- src/components/CommandPalette.tsx
+++ src/components/CommandPalette.tsx
@@ -9,91 +9,79 @@ import {
 import { useEditorState, type Editor } from "@tiptap/react";
 import { Toggle } from "./ui/toggle";
 
-
-const CommandPalette = ({editor}: {editor: Editor | null}) => {
-  if(!editor) return null;
+const CommandPalette = ({ editor }: { editor: Editor | null }) => {
+  if (!editor) return null;
 
   const editorState = useEditorState({
     editor,
-    selector: ctx => {
+    selector: (ctx) => {
       return {
-        isBold: ctx.editor.isActive('bold') ?? false,
-        isItalic: ctx.editor.isActive('italic') ?? false,
-        isStrike: ctx.editor.isActive('strike') ?? false,
-        isHeading1: ctx.editor.isActive('heading', { level: 1 }) ?? false,
-        isHeading2: ctx.editor.isActive('heading', { level: 2 }) ?? false,
-        isBulletList: ctx.editor.isActive('bulletList') ?? false,
-        isOrderedList: ctx.editor.isActive('orderedList') ?? false
-      }
+        isBold: ctx.editor.isActive("bold") ?? false,
+        isItalic: ctx.editor.isActive("italic") ?? false,
+        isStrike: ctx.editor.isActive("strike") ?? false,
+        isHeading1: ctx.editor.isActive("heading", { level: 1 }) ?? false,
+        isHeading2: ctx.editor.isActive("heading", { level: 2 }) ?? false,
+        isBulletList: ctx.editor.isActive("bulletList") ?? false,
+        isOrderedList: ctx.editor.isActive("orderedList") ?? false,
+      };
     },
-  })
+  });
 
   const bisActions = [
     {
       icon: <Bold className="size-5" />,
       label: "Bold Text",
-      onClick:() => editor.chain().focus().toggleBold().run(),
-      isPressed: editorState.isBold
+      onClick: () => editor.chain().focus().toggleBold().run(),
+      isPressed: editorState.isBold,
     },
     {
       icon: <Italic className="size-5" />,
       label: "Italicize Text",
       onClick: () => editor.chain().focus().toggleItalic().run(),
-      isPressed: editorState.isItalic
+      isPressed: editorState.isItalic,
     },
     {
       icon: <Strikethrough className="size-5" />,
       label: "Strikethrough Text",
       onClick: () => editor.chain().focus().toggleStrike().run(),
-      isPressed: editorState.isStrike
+      isPressed: editorState.isStrike,
     },
   ];
   const headings = [
     {
       icon: <Heading1 className="size-5" />,
       label: "Heading 1",
-      onClick:() => editor.chain().focus().toggleHeading({ level: 1 }).run(),
-      isPressed: editorState.isHeading1
+      onClick: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
+      isPressed: editorState.isHeading1,
     },
     {
       icon: <Heading2 className="size-5" />,
       label: "Heading 2",
-      onClick:() => editor.chain().focus().toggleHeading({ level: 2 }).run(),
-      isPressed: editorState.isHeading2
+      onClick: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
+      isPressed: editorState.isHeading2,
     },
-  ]
+  ];
+
+  const menu = [bisActions, headings];
   return (
     <div className="h-full flex flex-col gap-3 items-center justify-center px-9 z-50">
-      <div className="flex shadow-xl p-2 rounded-3xl bg-neutral-900/60">
-        <div className="flex flex-col gap-2">
-          {bisActions.map((item, index) => (
-            <Toggle
-              key={index}
-              size={"lg"}
-              pressed={item.isPressed}
-              onPressedChange={item.onClick}
-              className="rounded-xl"
-            >
-              {item.icon}
-            </Toggle>
-          ))}
+      {menu.map((item, index) => (
+        <div className="flex shadow-xl p-2 rounded-3xl bg-neutral-900/60">
+          <div key={index} className="flex flex-col gap-2">
+            {item.map((item, index) => (
+              <Toggle
+                key={index}
+                size={"lg"}
+                pressed={item.isPressed}
+                onPressedChange={item.onClick}
+                className="rounded-xl"
+              >
+                {item.icon}
+              </Toggle>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="flex shadow-xl p-2 rounded-3xl bg-neutral-900/60">
-        <div className="flex flex-col gap-2">
-          {headings.map((item, index) => (
-            <Toggle
-              key={index}
-              size={"lg"}
-              pressed={item.isPressed}
-              onPressedChange={item.onClick}
-              className="rounded-xl"
-            >
-              {item.icon}
-            </Toggle>
-          ))}
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
