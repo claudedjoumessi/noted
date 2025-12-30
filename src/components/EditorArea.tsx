7483fa79e44707f8d9ad3783748fc461126a25d4
claudedjoumessi <djoumessiclaude95@gmail.com>
2025-12-30 03:34:39 +0100

    feat: update EditorArea UI and spinner, and improve layout styling

--- src/components/EditorArea.tsx
+++ src/components/EditorArea.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useEditor, EditorContent, EditorContext } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import CommandPalette from "./CommandPalette";
+import { Spinner } from "./ui/spinner";
 
 const EditorArea = () => {
   const editor = useEditor({
@@ -9,21 +10,26 @@ const EditorArea = () => {
     content: "<h1>Hello World!</h1>", // initial content
     editorProps: {
       attributes: {
-        class: "w-full p-3 outline-none",
+        class: "w-full mt-4 outline-none",
       },
     },
   });
 
   // Memoizing context for efficiency
   const editorProvider = React.useMemo(() => ({ editor }), [editor]);
-  // const editorProvider = editor;
 
   return (
     <div className="w-dvw h-dvh flex">
       <EditorContext.Provider value={editorProvider}>
         <CommandPalette editor={editor} />
-        <div className="px-4 py-6 w-full h-full editor-area">
+        <div className="flex flex-col gap-1 px-4 py-6 w-full h-full editor-area">
           <EditorContent editor={editor} />
+          <div className="w-full h-auto flex gap-1.5 items-center text-base">
+            <span>Note 1 - Spreadsheets</span>
+            <b>&nbsp;&nbsp;&middot;&nbsp;&nbsp;</b>
+            <Spinner />
+            Saving...
+          </div>
         </div>
       </EditorContext.Provider>
     </div>
